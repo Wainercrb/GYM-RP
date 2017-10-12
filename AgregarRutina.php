9620ee7cff5d5991c9eb7ce70f1a751ef9461f59
WainerYGabriel <you@example.com>
2017-10-11 18:20:43 -0600

    se agregan más archivos php

--- AgregarRutina.php
+++ AgregarRutina.php
@@ -11,32 +11,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   </head>
   <body>
-    <div class="bs-example">
-      <nav class="navbar navbar-default">
-        <div class="navbar-header">
-          <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          </button>
-          <a href="#" class="navbar-brand">GYM-UTN</a>
-        </div>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Inicio</a></li>
-            <li><a href="#">Registro</a></li>
-            <li><a href="#">Salir</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><img id="image-profile" src="imagenes/aa.png" class="img-circle special-img" alt="profile" width="35px" height="35px"></a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Usuario</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
     <div class="container" id="main-container">
       <div class="row">
         <div class="col-md-6 col-md-offset-3">
@@ -130,11 +104,11 @@
     <footer>
       <div class="container">
         <p class="text-center">Create by <a href="http://www.facebook.com" rel="author">Wainer Rodriguez Bonilla</a> and  <a href="http://facebook.com" rel="author">Juan Gabriel Rosales</a>
-        </p>
-      </div>
-    </footer>
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/RegistroRutina.js"></script>
-  </body>
+      </p>
+    </div>
+  </footer>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/RegistroRutina.js"></script>
+</body>
 </html>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Login</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/estiloLogin.css">
+		<link rel="stylesheet" href="css/header.css">
+	</head>
+	<body>
+		<?php
+			include "header.php";
+		?>
+		<div class="login-wrap">
+			<div class="login-html">
+				<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Ingresar</label>
+				<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Registrase</label>
+				<div class="login-form">
+					<div class="sign-in-htm">
+						<div class="group">
+							<label for="user" class="label">Usuario</label>
+							<input id="user" type="text" class="input">
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Contraseña</label>
+							<input id="pass" type="password" class="input" data-type="password">
+						</div>
+						<div class="group">
+							<input id="check" type="checkbox" class="check" checked>
+							<label for="check"><span class="icon"></span> recordame</label>
+						</div>
+						<div class="group">
+							<input onclick="loginIngreso();" type="submit" class="button" value="Ingresar">
+						</div>
+						<div class="hr"></div>
+						<div class="foot-lnk">
+							<a href="#forgot">Olvido su contraseña?</a>
+						</div>
+					</div>
+					<div class="sign-up-htm">
+						<div class="group">
+							<label for="user" class="label">Usuario</label>
+							<input id="user" type="text" class="input">
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Contraseña</label>
+							<input id="pass" type="password" class="input" data-type="password">
+						</div>
+						
+						<div class="group">
+							<label for="pass" class="label">Email </label>
+							<input id="pass" type="text" class="input">
+						</div>
+						<div class="group">
+							<label  id="pass"for="combobox" class="label">Sexo</label>
+							<select  id="combobox" class="input"name="select">
+								<option value="value1">Masculino</option>
+								<option value="value2" >Femenino</option>
+								<option value="value3">Desconocido</option>
+							</select>
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Peso</label>
+							<input id="pass" type="password" class="input" data-type="text">
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Altura</label>
+							<input id="pass" type="password" class="input" data-type="text">
+						</div>
+						<div class="group">
+							<label for="pass" class="label">Talla</label>
+							<input id="pass" type="password" class="input" data-type="text">
+						</div>
+						<div class="group">
+							<input  id="pass" type="submit" class="button" value="Registarse ">
+						</div>
+						<div class="hr"></div>
+						<div class="foot-lnk">
+						<label for="tab-1"> Ya eres Mienbro?</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script language="JavaScript" type="text/javascript" src="js/direccionamiento.js"></script>
+</body>
+</html>
